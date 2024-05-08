6e7fda5faf2453c73c06d6bc1cc96009a0b5ea08
Mahesh Pasaladi <127716009+Maheshpasaladi14@users.noreply.github.com>
2024-05-08 14:14:48 +0530

    Add files via upload

---
+++ client/src/components/ForFooter/Footer.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+export default function Footer() {
+  return (
+    <div className="footer">
+      <div className="footer-content">
+        <p>&copy; 2024 Job Portal</p>
+      </div>
+      <div className="footer-author">
+        <p>Made with ❤️ by Mahesh Pasaladi</p>
+      </div>
+    </div>
+  );
+}
